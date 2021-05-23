066a54fc3af04c7a3e38bdc72874181ceb6c1277
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-24 01:10:03 +0900

    八百屋プログラム（メソッド）終了

--- greengrocer2.rb
+++ greengrocer2.rb
@@ -11,3 +11,5 @@ disp_products(products)
 chosen_product = choose_product(products)
 quantity_of_product = decide_quantity(chosen_product)
 calculate_charges(chosen_product, quantity_of_product)
+
+# 変数を利用できるスコープを意識する必要があり難しかった。
