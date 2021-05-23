0ca207468e5b4d10516742cad5489e3e86a7d468
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-24 00:12:00 +0900

    八百屋プログラム（条件分岐）終了

--- greengrocer.rb
+++ greengrocer.rb
@@ -6,11 +6,31 @@ products = [
 ]
 
 puts "いらっしゃいませ！商品を選んでください。"
-products.each.with_index do |product, i|
+products.each.with_index(1) do |product, i|
   puts "#{i}.#{product[:name]}(#{product[:price]}円)"
 end
 
-print "商品の番号を選択 > "
-select_product_num = gets.to_i
+while true
+  print "商品の番号を選択 > "
+  select_product_num = gets.to_i
+  break if (1..4).include?(select_product_num)
+  puts "1~4の番号を入力してください。"
+end
 
 chose_product = products[select_product_num - 1]
+
+puts "#{chose_product[:name]}ですね。何個買いますか？"
+
+while true
+  print "個数を入力 > "
+  quantity_of_product = gets.to_i
+  break if quantity_of_product >= 1
+  puts "1個以上選んでください。"
+end
+
+total_price = chose_product[:price] * quantity_of_product
+if quantity_of_product >= 5
+  total_price *= 0.9
+end
+puts "#{total_price.floor}円です。"
+puts "お買い上げありがとうございました！"
